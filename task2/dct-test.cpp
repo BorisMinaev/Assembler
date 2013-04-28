d6ed055d690fb01771177994230c30f4324a3a6e
Boris Minaev <qwerty787788@qwerty787788.(none)>
2013-04-28 17:56:15 +0400

    add some test functions

--- task2/dct-test.cpp
+++ task2/dct-test.cpp
@@ -8,6 +8,8 @@ extern "C"
 	void FDCT(float* data, float* res, int n);
 	void transpose(float* data, float* res);
 	void copy(float* data, float* res);
+	void matrix_mul(float* data1, float * data2, float* res);
+	void matrix_mul3(float* data1, float * data2, float* res);
 
 }
 
@@ -20,14 +22,35 @@ void transpose2(float * data, float * res) {
 			res[i * 8 + j] = data[j * 8 + i];
 }
 
+void matrix_mul2(float * m1, float * m2, float * res) {
+	float zz;
+	for (int i =  0; i < 8; i++) 
+		for (int j = 0; j < 8; j++) {
+			zz =0;
+			for (int  k = 0; k < 8; k++)
+				zz += m1[i * 8 + k] * m2[j * 8 + k];
+			res[i * 8 + j] = zz;
+		}
+}
+
 int main ( )
 {
-	float data[SIZE * SIZE * N];
+	float data1[SIZE * SIZE * N];
 	int it = 0;
 	for (int i = 0; i < N; i++) {
 		for (int x = 0; x < SIZE; x++) {
 			for (int y = 0; y < SIZE; y++) {
-				data[it++] = rand() / 10000000.0;
+				data1[it++] = rand() / 1000000000.0;
+			}
+		}
+	}
+
+	float data2[SIZE * SIZE * N];
+	it = 0;
+	for (int i = 0; i < N; i++) {
+		for (int x = 0; x < SIZE; x++) {
+			for (int y = 0; y < SIZE; y++) {
+				data2[it++] = rand() / 1000000000.0;
 			}
 		}
 	}
@@ -46,8 +69,8 @@ int main ( )
 	clock_t t1, t2;
 
  	t1 = clock();  
-	for (int it = 0; it < 10000000; it++) {
-		copy(data, res);
+	for (int it = 0; it < 1; it++) {
+		matrix_mul(data1, data2, res);
 	}
 	t2 = clock();
 	float diff = (((float)t2 - (float)t1) / 1000000.0F ) * 1000;   
--- task2/dct.asm
+++ task2/dct.asm
@@ -8,6 +8,8 @@ section .text
 global FDCT
 global transpose
 global copy
+global matrix_mul
+global matrix_mul3
 
 ; void DFCT (float* data, float* res, int n)
 FDCT:  
@@ -82,5 +84,154 @@ copy:
         cmp ecx, 32
         jne _loop_ecx2
 
+  
+ret
+
+
+; void matrix_mul (float* data1, float * data2, float* res)
+matrix_mul:  
+    rdtsc
+    push eax
+    mov eax, [esp + 8]; data1
+    mov ebx, [esp + 12]; data2
+    mov ecx, [esp + 16]; data3
+
+
+    mov dl, 0
+    _loop_dl:
+        mov dh, 0
+        mov ebx, [esp + 12]
+        _loop_dh:
+            push 0.0
+            movss xmm0, [esp]
+            add esp, 4
+
+            ; 0
+            movss xmm1, [eax]
+            movss xmm2, [ebx]
+            mulss xmm1, xmm2
+            addss xmm0, xmm1
+            ; 1
+            movss xmm1, [eax + 4]
+            movss xmm2, [ebx + 4]
+            mulss xmm1, xmm2
+            addss xmm0, xmm1
+            ; 2
+            movss xmm1, [eax + 8]
+            movss xmm2, [ebx + 8]
+            mulss xmm1, xmm2
+            addss xmm0, xmm1
+            ; 3
+            movss xmm1, [eax + 12]
+            movss xmm2, [ebx + 12]
+            mulss xmm1, xmm2
+            addss xmm0, xmm1
+
+            ; 0
+            movss xmm1, [eax + 16]
+            movss xmm2, [ebx + 16]
+            mulss xmm1, xmm2
+            addss xmm0, xmm1
+            ; 1
+            movss xmm1, [eax + 20]
+            movss xmm2, [ebx + 20]
+            mulss xmm1, xmm2
+            addss xmm0, xmm1
+            ; 2
+            movss xmm1, [eax + 24]
+            movss xmm2, [ebx + 24]
+            mulss xmm1, xmm2
+            addss xmm0, xmm1
+            ; 3
+            movss xmm1, [eax + 28]
+            movss xmm2, [ebx + 28]
+            mulss xmm1, xmm2
+            addss xmm0, xmm1
+
+            ;movss xmm1, [eax + ecx * 8]
+
+            movss [ecx], xmm0
+
+            add ebx, 32
+            add ecx, 4
+            add dh, 4
+            cmp dh, 32
+            jne _loop_dh
+        
+        add eax, 32
+        add dl, 4
+        cmp dl, 32
+        jne _loop_dl
+
+
+    rdtsc
+    sub eax, [esp]
+    add esp, 4
+
+    push eax
+    push print_int
+    call printf
+    add esp, 8
+  
+ret
+
+; void matrix_mul3 (float* data1, float * data2, float* res)
+matrix_mul3:  
+    rdtsc
+    push eax
+    mov eax, [esp + 8]; data1
+    mov ebx, [esp + 12]; data2
+    mov ecx, [esp + 16]; data3
+
+
+    mov dl, 0
+    _loop_dl2:
+        mov dh, 0
+        mov ebx, [esp + 12]
+        _loop_dh2:
+            push 0.0
+            push 0.0
+            push 0.0
+            push 0.0
+            movups xmm0, [esp]
+            add esp, 16
+
+            ; first 4 numbers
+            movups xmm1, [eax]
+            movups xmm2, [ebx]
+            mulps xmm1, xmm2
+            addps xmm0, xmm1
+ 
+            ; last 4 numbers  
+            movups xmm1, [eax + 16]
+            movups xmm2, [ebx + 16]
+            mulps xmm1, xmm2
+            addps xmm0, xmm1
+
+            ; sum all numvers
+            haddps xmm0, xmm0;
+            haddps xmm0, xmm0
+
+            movss [ecx], xmm0
+
+            add ebx, 32
+            add ecx, 4
+            add dh, 4
+            cmp dh, 32
+            jne _loop_dh2
+        
+        add eax, 32
+        add dl, 4
+        cmp dl, 32
+        jne _loop_dl2
+
+    rdtsc
+    sub eax, [esp]
+    add esp, 4
+
+    push eax
+    push print_int
+    call printf
+    add esp, 8
   
 ret
