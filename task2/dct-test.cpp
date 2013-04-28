7435ec9210c83f99141ab626f163460ed11a2c8a
Boris Minaev <qwerty787788@qwerty787788.(none)>
2013-04-28 22:58:55 +0400

    working version

--- task2/dct-test.cpp
+++ task2/dct-test.cpp
@@ -5,7 +5,8 @@
 extern "C"
 {
 
-	void FDCT(float* data, float* res, int n);
+	void _fdct(float* data, float* res, int n);
+	void _idct(float* data, float* res, int n);
 	void transpose(float* data, float* res);
 	void time_test(float* data, float* res);
 	void copy(float* data, float* res);
@@ -16,7 +17,7 @@ extern "C"
 
 }
 
-const int N = 1;
+const int N = 2;
 const int SIZE = 8;
 
 void transpose2(float * data, float * res) {
@@ -26,18 +27,19 @@ void transpose2(float * data, float * res) {
 }
 
 void matrix_mul2(float * m1, float * m2, float * res) {
-	float zz;
+	for (int i = 0; i < 64; i++)
+		res[i] = 0;
 	for (int i =  0; i < 8; i++) 
 		for (int j = 0; j < 8; j++) {
-			zz =0;
-			for (int  k = 0; k < 8; k++)
-				zz += m1[i * 8 + k] * m2[j * 8 + k];
-			res[i * 8 + j] = zz;
+			for (int k = 0; k < 8; k++) {
+				res[i*8+k] += m1[i * 8 +j]  * m2[j * 8 + k];
+			}
 		}
 }
 
 int main ( )
 {
+	/*
 	float data1[SIZE * SIZE * N];
 	int it = 0;
 	for (int i = 0; i < N; i++) {
@@ -93,4 +95,53 @@ int main ( )
 		printf("\n");
 	}
 	
+	float matr1[64];
+	float matr2[64];
+	int N = 8;
+	float pi = atan(1) * 4;
+	for (int j = 0; j < 8; j++) {
+		for (int i = 0; i < 8; i++) {
+			float val;
+			if (i == 0) {
+				val = 1 / sqrt(N + 0.);
+			} else {
+				val = sqrt(2. / (N + 0.)) * cos((2 * j + 1) * i * pi/2./(N + 0.));
+			}
+			val /= sqrt(8);
+			matr1[i * 8 + j] = matr2[j * 8 + i] = val;
+			printf("%.1f, ", 0.0);
+		}
+		//std::cout << std::endl;
+	}
+	
+*/
+
+	float cur[] = {-16342,   2084, -10049,  10117,   2786,   -659,  -4905,  12975,
+ 10579,   8081, -10678,  11762,   6898,    444,  -6422, -15892,
+-13388,  -4441, -11556, -10947,  16008,  -1779, -12481, -16230,
+-16091,  -4001,   1038,   2333,   3335,   3512, -10936,   5343,
+ -1612,  -4845, -14514,   3529,   9284,   9916,    652,  -6489,
+ 12320,   7428,  14939,  13950,   1290, -11719,  -1242,  -8672,
+ 11870,  -9515,   9164,  11261,  16279,  16374,   3654,  -3524,
+ -7660,  -6642,  11146, -15605,  -4067, -13348,   5807, -14541};
+ 	float res2[64 * N];
+ 	float arr1[64 * N];
+ 	for (int i = 0; i < 64 * N ;i++)
+ 		arr1[i] = cur[i % 64];
+
+ 	_fdct(arr1, res2, N);
+ 	//matrix_mul2(cur, cur, res2);
+
+ 	int n = 8;
+ 	for (int ii = 0; ii < N; ii++) {
+ 	for (int i = 0; i < n; i++) {
+ 		for (int j =0; j < n; j++) {
+ 			printf("%.2f ", res2[ii * 64 + i * 8 + j]);
+ 		}
+ 		std::cout << std::endl;
+ 	}
+ 	std::cout << std::endl;
+ }
+
+ 	
 }
--- task2/dct.asm
+++ task2/dct.asm
@@ -1,11 +1,25 @@
 extern printf
 
 section .data
+    align 16
     print_int db "%d", 10, 0
+
+    hw db "Hello, world", 10, 0
+
+    align 16
+    matr1: dd 0.12500000, 0.12500000, 0.12500000, 0.12500000, 0.12500000, 0.12500000, 0.12500000, 0.12500000, 0.17337997, 0.14698444, 0.09821186, 0.03448742, -0.03448743, -0.09821188, -0.14698446, -0.17337999, 0.16332036, 0.06764951, -0.06764952, -0.16332038, -0.16332036, -0.06764949, 0.06764954, 0.16332039, 0.14698444, -0.03448743, -0.17337999, -0.09821185, 0.09821189, 0.17337997, 0.03448739, -0.14698447, 0.12500000, -0.12500001, -0.12499999, 0.12500001, 0.12499998, -0.12500003, -0.12499996, 0.12500004, 0.09821186, -0.17337999, 0.03448744, 0.14698444, -0.14698447, -0.03448737, 0.17337996, -0.09821194, 0.06764951, -0.16332036, 0.16332039, -0.06764955, -0.06764946, 0.16332035, -0.16332039, 0.06764959, 0.03448742, -0.09821185, 0.14698444, -0.17337997, 0.17338000, -0.14698449, 0.09821194, -0.03448752
+
+    align 16
+    matr2: dd 0.12500000, 0.17337997, 0.16332036, 0.14698444, 0.12500000, 0.09821186, 0.06764951, 0.03448742, 0.12500000, 0.14698444, 0.06764951, -0.03448743, -0.12500001, -0.17337999, -0.16332036, -0.09821185, 0.12500000, 0.09821186, -0.06764952, -0.17337999, -0.12499999, 0.03448744, 0.16332039, 0.14698444, 0.12500000, 0.03448742, -0.16332038, -0.09821185, 0.12500001, 0.14698444, -0.06764955, -0.17337997, 0.12500000, -0.03448743, -0.16332036, 0.09821189, 0.12499998, -0.14698447, -0.06764946, 0.17338000, 0.12500000, -0.09821188, -0.06764949, 0.17337997, -0.12500003, -0.03448737, 0.16332035, -0.14698449, 0.12500000, -0.14698446, 0.06764954, 0.03448739, -0.12499996, 0.17337996, -0.16332039, 0.09821194, 0.12500000, -0.17337999, 0.16332039, -0.14698447, 0.12500004, -0.09821194, 0.06764959, -0.03448752
+
+    align 16
+    tmp: dd 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0
+
 
 section .text
 
-global FDCT
+global _fdct
+global _idct
 global transpose
 global copy
 global copy2
@@ -14,28 +28,95 @@ global matrix_mul3
 global matrix_mul4
 global time_test
 
-; void DFCT (float* data, float* res, int n)
-FDCT:  
+; void _fdct (float* data, float* res, int n)
+_fdct:  
     mov eax, [esp + 4]; data
     mov ebx, [esp + 8]; result
 
     mov edx, [esp + 12]
     _main_loop:
+        pusha
+            push tmp
+            push eax
+            push matr1
+            call matrix_mul4    
+            add esp, 12
+        popa
 
-        mov ecx, 64
-        _loop:
-            movss xmm1, [eax]
-            movss[ebx], xmm1
-            add ebx, 4
-            add eax, 4
-            loop _loop, ecx
+        pusha
+            push ebx
+            push matr2 
+            push tmp
+            call matrix_mul4
+            add esp, 12
+        popa
 
+        add eax, 64 * 4
+        add ebx, 64 * 4
         sub edx, 1
         jnz _main_loop
   
 ret
 
+; void _idct (float* data, float* res, int n)
+_idct:  
+    mov eax, [esp + 4]; data
+    mov ebx, [esp + 8]; result
+    mov ecx, [esp + 16]
 
+    mov edx, [esp + 12]
+    _main_loop2:
+        pusha
+            push tmp
+            push eax
+            push matr2
+            call matrix_mul4    
+            add esp, 12
+        popa
+
+        pusha
+            push ebx
+            push matr1 
+            push tmp
+            call matrix_mul4
+            add esp, 12
+        popa
+
+        pusha
+            push ebx
+            call mul64
+            add esp, 4
+        popa
+
+
+        add eax, 64 * 4
+        add ebx, 64 * 4
+        sub edx, 1
+        jnz _main_loop2
+  
+ret
+
+
+; void mul64 (float * data) 
+mul64:
+    mov eax, [esp + 4]
+    mov ecx, 16
+    push 64.0
+    push 64.0
+    push 64.0
+    push 64.0
+    movups xmm0, [esp]
+    add esp, 16
+    _loop_mul64:
+        movaps xmm1, [eax];
+        mulps xmm1, xmm0
+        movaps [eax], xmm1
+        add eax, 16
+        sub ecx, 1
+        jnz _loop_mul64
+
+
+ret
 ; void time_test (float* data, float* res)
 time_test:  
     mov ecx, [esp + 4]
@@ -363,13 +444,8 @@ matrix_mul4:
     movups xmm7, [esp]
     add esp, 16
 
-    rdtsc
-    push eax
-
-    mov esi, 1000
-    lloopp6:
-    mov eax, [esp + 8]; data1
-    mov edx, [esp + 16]; res
+    mov eax, [esp + 4]; data1
+    mov edx, [esp + 12]; res
 
     ; matrix mul
     mov cl, 0
@@ -377,7 +453,7 @@ matrix_mul4:
         ; first part
         movaps xmm0, xmm7
         
-        mov ebx, [esp + 12]
+        mov ebx, [esp + 8]
         mov ch, 0
         _loop_mul2:
             movss xmm1, [eax]
@@ -397,7 +473,7 @@ matrix_mul4:
         sub eax, 32
         ; second part
         movaps xmm0, xmm7
-        mov ebx, [esp + 12]
+        mov ebx, [esp + 8]
         mov ch, 0
         _loop_mul22:
             movss xmm1, [eax]
@@ -418,18 +494,5 @@ matrix_mul4:
         inc cl
         cmp cl, 8
         jne _loop_mul1
-
-    ; matrix mul end
-    sub esi, 1
-    jnz lloopp6
-    
-    rdtsc
-    sub eax, [esp]
-    add esp, 4
-
-    push eax
-    push print_int
-    call printf
-    add esp, 8
-  
+ 
 ret
